68ef4b12d6e2ce4590c854d81799e7bad47fecc4
Ismael Flores Meléndez <ismaelfm1998@hotmail.com>
2019-03-07 02:22:24 -0600

    Identifications of error works
    Need to reorder the matrix to get lineal independence

--- Tareas/Metodo de Jaboc/metodo_jacob.py
+++ Tareas/Metodo de Jaboc/metodo_jacob.py
@@ -1,38 +1,64 @@
+################################     
         #Var asignation
-    # Value matriz
-row1=[4,-1,1,3]
-row2=[2,1,5,3]
-row3=[-1,4,1,-2]
-matriz=[row1,row1,row3]
+    # Value matrix
+row0=[4,-1,1,3]
+row1=[2,1,5,3]
+row2=[-1,4,1,-2]
+matrix=[row0,row1,row2]
 
+    #Diferent Values
+VLI = False  # VALIDATE LINEAL INDEPENDENCE
+VLI_items = [False,False,False]
+
+#################################
         # Functions
 
-    # Print Matriz
-def PrintMatriz():
-    print ("\tMatriz Orinal")
+    # Print matrix
+def Printmatrix():
+    print ("\tOrigninal Matrix")
     for i in range(3):
         for j in range(4):
-            if j!=3:
-                print(str(matriz[i][j]),"\t", end="")
-            else:
-                print("=\t",str(matriz[i][j]))        
+            if j!=3: print(str(matrix[i][j]),"\t", end="")
+            else: print("=\t",str(matrix[i][j]))        
     print("",end="\n")
-
+    
+    #Error message by item, number, descripcions
 def MensajesError(ele,num,error):
     print(ele,num,error)
 
     # Linealmente independiente
 def VerifyLinealIndep():    
-    if ( (abs(matriz[0][0])) < (( abs(matriz[0][1]) + abs(matriz[0][2]) )) ):
+        #Validate zero Column
+    if ( (abs(matrix[0][0])) < (( abs(matrix[0][1]) + abs(matrix[0][2]) )) ):
         MensajesError("La fila ",0," no cumple la regla de linealidad")
-    if ( (abs(matriz[1][1])) < (( abs(matriz[1][0]) + abs(matriz[1][2]) )) ):
-        MensajesError("La fila ",1," no cumple la regla de linealidad")
-    if ( (abs(matriz[2][2])) < (( abs(matriz[2][0]) + abs(matriz[2][1]) )) ):
-        MensajesError("La fila ",2," no cumple la regla de linealidad")
-    
-    
+        VLI_items[0] = False
+    else: VLI_items[0] = True
 
+        #Validate first Column
+    if ( (abs(matrix[1][1])) < (( abs(matrix[1][0]) + abs(matrix[1][2]) )) ):
+        MensajesError("La fila ",1," no cumple la regla de linealidad")
+        VLI_items[1] = False
+    else: VLI_items[1] = True
+        
+        #Validate second Column
+    if ( (abs(matrix[2][2])) < (( abs(matrix[2][0]) + abs(matrix[2][1]) )) ):
+        MensajesError("La fila ",2," no cumple la regla de linealidad")
+        VLI_items[2] = False
+    else: VLI_items[2] = True
+
+        #Validate All rows
+    if ( (VLI_items[0]==True) and (VLI_items[1]==True) and (VLI_items[2]==True) ): VLI = True
+
+    #Try to fix order of matrix
+def RepairIndependence():
+    print("Some goint to happend here")
+
+
+################################
         #main
     # funtions calls
-PrintMatriz()
-VerifyLinealIndep()
+Printmatrix()
+while (VLI==False):
+    VerifyLinealIndep()
+    if VLI==False:
+        RepairIndependence()
