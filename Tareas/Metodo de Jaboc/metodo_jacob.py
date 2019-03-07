87a86ce4b94ea02d7ba2d987657dc1bbe24504dd
Ismael Flores Meléndez <ismaelfm1998@hotmail.com>
2019-03-07 01:22:26 -0600

    Verificación Linealmente Independientes

--- Tareas/Metodo de Jaboc/metodo_jacob.py
+++ Tareas/Metodo de Jaboc/metodo_jacob.py
@@ -9,19 +9,28 @@ matriz=[row1,row1,row3]
 
     # Print Matriz
 def PrintMatriz():
+    print ("\tMatriz Orinal")
     for i in range(3):
         for j in range(4):
             if j!=3:
                 print(str(matriz[i][j]),"\t", end="")
             else:
-                print(str(matriz[i][j]))        
+                print("=\t",str(matriz[i][j]))        
     print("",end="\n")
 
+def MensajesError(ele,num,error):
+    print(ele,num,error)
+
     # Linealmente independiente
-def VerifyLinealIndep():
-    print("It´s works")
-
-
+def VerifyLinealIndep():    
+    if ( (abs(matriz[0][0])) < (( abs(matriz[0][1]) + abs(matriz[0][2]) )) ):
+        MensajesError("La fila ",0," no cumple la regla de linealidad")
+    if ( (abs(matriz[1][1])) < (( abs(matriz[1][0]) + abs(matriz[1][2]) )) ):
+        MensajesError("La fila ",1," no cumple la regla de linealidad")
+    if ( (abs(matriz[2][2])) < (( abs(matriz[2][0]) + abs(matriz[2][1]) )) ):
+        MensajesError("La fila ",2," no cumple la regla de linealidad")
+    
+    
 
         #main
     # funtions calls
