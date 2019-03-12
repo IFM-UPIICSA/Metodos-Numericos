556354bd84a6b6197208103ed66e932976c83f27
Ismael Flores Meléndez <ismaelfm1998@hotmail.com>
2019-03-11 23:48:08 -0600

    Resuelto el método
    Teniendo los valores por gauus, lo unico que se hizo fue transponer los resultados en valores de acuerdo al polinomio

--- Metodos/Interpolación Simple/inter_simple.py
+++ Metodos/Interpolación Simple/inter_simple.py
@@ -13,7 +13,15 @@ from gauss_method import gaussJordan
 A = [ [ 2.56, 1.6, 1], [ 3.24, 1.8, 1], [ 1.96, 1.4, 1] ]
 # Matriz de resultados
 b = [1.6487,2.7182,1]
+# Valor a calcular
+x = 1.7
 
-    # Resolución de la matriz
+    # Resolución de la matriz por Gauss Jordan
 R = (gaussJordan(A, b))
 print(R)
+
+    # Realizar el calculo del valor en función de los valores de la matriz
+    # y el polinomio a emplear
+# Polinomio de grado 3
+Resultado = ( R[0]*(x**2) ) + ( R[1]*(x) )+ ( R[2] )
+print(round(Resultado,4))
