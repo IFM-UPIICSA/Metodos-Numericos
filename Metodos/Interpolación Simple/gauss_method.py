0ef07f17f00198e0695fbe0cbbceff5aa9bc1504
Ismael Flores Meléndez <ismaelfm1998@hotmail.com>
2019-03-11 23:35:49 -0600

    Avance Inserción de método
    Se importa el archivo de Gauss para importar su método y tras adaptarlo poder tener la solución de la matriz que le pasemos desde Interpolación

---
+++ Metodos/Interpolación Simple/gauss_method.py
@@ -0,0 +1,24 @@
+import numpy
+def gaussJordan(matriz, vector):
+
+    matrix = numpy.array(matriz, dtype=numpy.float64)
+    vector = numpy.array(vector, dtype=numpy.float64)
+
+    m = len(vector)
+    x = numpy.zeros(m)
+
+    for k in range(0, m):
+        for r in range(k+1, m):
+            factor=(matrix[r,k]/matrix[k,k])
+            vector[r]=vector[r]-(factor*vector[k])
+            for c in range(0,m):
+                matrix[r,c]=matrix[r,c]-(factor*matrix[k,c])
+
+    x[m-1]=vector[m-1]/matrix[m-1, m-1]
+
+    for r in range(m-2, -1, -1):
+        suma = 0
+        for c in range(0,m):
+            suma=suma+matrix[r,c]*x[c]
+        x[r]=(vector[r]-suma)/matrix[r, r]  
+    return x
--- Metodos/Interpolación Simple/inter_simple.py
+++ Metodos/Interpolación Simple/inter_simple.py
@@ -6,6 +6,7 @@
 
 # imports
 import numpy
+from gauss_method import gaussJordan
 
     #Declaraciones
 # Matriz de coeficientes
@@ -14,29 +15,5 @@ A = [ [ 2.56, 1.6, 1], [ 3.24, 1.8, 1], [ 1.96, 1.4, 1] ]
 b = [1.6487,2.7182,1]
 
     # Resolución de la matriz
-def gaussJordan(matriz, vector):
-
-    matrix = numpy.array(matriz, dtype=numpy.float64)
-    vector = numpy.array(vector, dtype=numpy.float64)
-
-    m = len(vector)
-    x = numpy.zeros(m)
-
-    for k in range(0, m):
-        for r in range(k+1, m):
-            factor=(matrix[r,k]/matrix[k,k])
-            vector[r]=vector[r]-(factor*vector[k])
-            for c in range(0,m):
-                matrix[r,c]=matrix[r,c]-(factor*matrix[k,c])
-
-    x[m-1]=vector[m-1]/matrix[m-1, m-1]
-
-    for r in range(m-2, -1, -1):
-        suma = 0
-        for c in range(0,m):
-            suma=suma+matrix[r,c]*x[c]
-        x[r]=(vector[r]-suma)/matrix[r, r]  
-    return x
-    
 R = (gaussJordan(A, b))
 print(R)
