267ba49cefd973419b32febd238b418d28d8205c
Ismael Flores Meléndez <ismaelfm1998@hotmail.com>
2019-03-23 12:55:54 -0600

    modificaciones

--- Proyecto/inter_simple_3.py
+++ Proyecto/inter_simple_3.py
@@ -6,22 +6,23 @@
 
 # imports
 import numpy
-from gauss_method import gaussJordan
+from metodos.gauss_method import gaussJordan
 
-    #Declaraciones
-# Matriz de coeficientes
+###    Declaraciones
+#Matriz de coeficientes
 A = [[ 4.0960, 2.56, 1.6, 1] , [5.8320, 3.24, 1.8, 1], [ 2.7440, 1.96, 1.4, 1], [8, 4, 2, 1]]
 # Matriz de resultados
 b = [1.6487,2.7182,1, 7.3891]
 # Valor a calcular
 x = 1.9
+###
 
     # Resolución de la matriz por Gauss Jordan
 R = (gaussJordan(A, b))
+print("\nSolición de la matriz por Gauss Jordan")
 print(R)
 
-    # Realizar el calculo del valor en función de los valores de la matriz
-    # y el polinomio a emplear
+    # Calculo del valor en función de los valores de la matriz y el polinomio a emplear
 # Polinomio de grado 3
 Resultado = ( R[0]*(x**3) ) + ( R[1]*(x**2) ) + ( R[2]*(x) )+ ( R[3] )
-print(round(Resultado,4))
+print("\nResultado del polinomio ", round(Resultado,8),"\n")
