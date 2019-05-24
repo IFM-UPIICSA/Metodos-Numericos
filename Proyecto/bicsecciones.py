b4cca76989ec48bbb6ce8b57511d6ce3b2c21177
Ismael Flores Melendez <ismaelfm1998@hotmail.com>
2019-05-24 14:12:39 -0500

    Funciona!! pero con errores :(

--- Proyecto/bicsecciones.py
+++ Proyecto/bicsecciones.py
@@ -19,8 +19,8 @@ print("\n\tDefina la función: \n")
 grade = int(input("Ingrese el grado de la funcion: "))+1
 function_value = function_grade_n(grade)
 print("\n\tIngrese el limite:\n")
-x_inf = (int(input("Ingresa el valor inferior: ")))   
-x_sup = (int(input("Ingresa el valor superior: ")))
+x1 = (int(input("Ingresa el valor inferior: ")))   
+x2 = (int(input("Ingresa el valor superior: ")))
     #Imprimiendo datos ingresados
 print("\n\tFuncion:\n")
 for i in range(grade-1,-1,-1):
@@ -28,16 +28,54 @@ for i in range(grade-1,-1,-1):
         print("X^",i," = ",function_value[i])
     else:
         print("Termino independiente = ", function_value[i])
-print("\n\tLimites: \n[",x_inf,",",x_sup,"]")
-
+print("\n\tLimites: \n[",x1,",",x2,"]")
+    #Solicitud del fix
 fix_value= (int(input("\nIngrese el fix: ")))
-cantidad = 40
-    #Calcular iteraciones
-while ( cantidad > (5/(10**fix_value))):
-    print("Previo: ",round(cantidad, fix_value))
-    cantidad = cantidad/10
-    print("Post: ",round(cantidad, fix_value))
-print("\n\n\n")
-print (round(cantidad,fix_value))
-    #Imprimir tabla
-#print("\n\nResultado:\n|Iteracion|X1|X2|Xmedia|f(X1)|f(X2)|f(Xmedia)|Error|\n")
+        #Calcular y mostrar las iteraciones
+    #Definicion de variables
+error_value = 1
+contador = 0
+    #Barra titular
+print("n\tX1\t\tX2\tXmedia\tf(X1)\tf(X2)\tf(Xmedia)\tError |f(Xmedia)|")
+    #Iteraciones
+while ( error_value > (5/(10**fix_value))):
+        #Init values
+    Xmedia = 0
+    fX1 = 0
+    fX2 = 0
+    fXmedia = 0
+        #Calcular Xmedia
+    Xmedia = ( (x1+x2) / 2 )
+        #Calcular fx1
+    for i in range(grade-1,-1,-1):
+        if(i!=0):
+            fX1 += ( function_value[i] * (x1**i))
+        else:
+            fX1 += (function_value[i])
+
+        #Calcular fx2
+    for i in range(grade-1,-1,-1):
+        if(i!=0):
+            fX2 += ( function_value[i] * (x2**i))
+        else:
+            fX2 += (function_value[i])
+
+        #Calcular fXmedai
+    for i in range(grade-1,-1,-1):
+        if(i!=0):
+            fXmedia += ( function_value[i] * (Xmedia**i))
+        else:
+            fXmedia += (function_value[i])
+
+        #Calcular error
+    error_value = abs(fXmedia)
+
+        #Imprimir iteracion
+    print(contador,"\t",round(x1,fix_value),"\t",round(x2,fix_value),"\t",round(Xmedia,fix_value),"\t",round(fX1,fix_value),"\t",round(fX2,fix_value),"\t",round(fXmedia,fix_value),"\t\t",round(error_value,fix_value))
+    if(fXmedia < 0):
+        x1 = Xmedia
+    else:
+        x2 = Xmedia
+    
+    contador = contador+1
+    
