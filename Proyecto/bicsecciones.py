286867435caa03d312d8e31750786be90c4f8218
Ismael Flores Melendez <ismaelfm1998@hotmail.com>
2019-05-24 11:28:31 -0500

    Bisecciones: Ingreso completo de datos
    Solicitud correcta
    
    SIguente: 
    Calculo de las tablas

--- Proyecto/bicsecciones.py
+++ Proyecto/bicsecciones.py
@@ -11,9 +11,15 @@ from funciones.solitud_datos import sol_No_Datos
 print("Metodo de Bisecciones")
 
     #Solicitud de datos
-print("Defina la función")
+print("\n\tDefina la función:\n")
 x_cuadrada = (int(input("Ingresa el valor de X^2: ")))
 x_uno = (int(input("Ingresa el valor de X^1: ")))
 x_cero = (int(input("Ingresa el valor de X^0: ")))
+print("\n\tIngrese el limite:\n")
+x_inf = (int(input("Ingresa el valor inferior: ")))   
+x_sup = (int(input("Ingresa el valor superior: ")))
+
     #Imprimiendo la funcion deseada
-print("Funcion: (",x_cuadrada,"X^2) + (",x_uno,"X) +(",x_cero,")")
+print("\nFuncion: \n\t(",x_cuadrada,"X^2) + (",x_uno,"X) +(",x_cero,")")
+print("\nLimites: \n\t[",x_inf,",",x_sup,"]")
+
