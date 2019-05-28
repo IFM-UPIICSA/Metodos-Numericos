694a26e67b9628d3685ca127af7352fe70918d2d
Ismael Flores Melendez <ismaelfm1998@hotmail.com>
2019-05-28 01:45:55 -0500

    Finish Polinomio de Newton

--- Proyecto/poli_newton.py
+++ Proyecto/poli_newton.py
@@ -6,8 +6,9 @@
 #############################
     #Imports
 import numpy as np
+import math
 from funciones.solitud_datos import enters
-from funciones.solitud_datos import sol_tabla_2n, sol_No_Datos
+from funciones.solitud_datos import sol_tabla_2n, sol_No_Datos, sol_Value_Calcular
 
 def Presentacion():
     enters(2)
@@ -39,6 +40,47 @@ def Contruction_Table(init_table, num_data):
                 tabla_creation[i][j] = (tabla_creation[i][j-1]-tabla_creation[i-1][j-1])
     return tabla_creation
 
+def Search_Menor(tabla_values, value_calcular, num_data):
+    menor = tabla_values[0][0]
+    for i in range(0, num_data):
+        if((value_calcular>tabla_values[i][0])):
+            menor = tabla_values[i][0]
+        else:
+            break
+    return menor
+
+def Buscar_Inicio(tabla_values, menor, num_data):
+    inicio = 0
+    for i in range(0, num_data):
+        if( menor == tabla_values[i][0] ):
+            inicio = i
+            break
+    return inicio
+
+def Calcular_Dividendo(k, num_veces):
+    valor_calculado = k
+    if num_veces>1:
+        for ii in range (1, num_veces):
+            valor_calculado *= (k-ii)
+    return float(valor_calculado)
+
+def Calcular_Result(tabla_values, value_calcular, num_data):
+    result = 0 #definir resultado
+    menor = Search_Menor(tabla_values, value_calcular, num_data) #calcular menor
+    k = round( (value_calcular-menor)/(tabla_values[1][0] - tabla_values[0][0]),5) #calcular k
+    print("\nMenor: ",menor," K: ",k)
+        #Calcular el inicio de la tabla
+    inicio = Buscar_Inicio(tabla_values, menor, num_data)
+        #Sumar resultados
+    result = tabla_values[inicio][1] #default
+    num_veces = 1
+    print("\nTabla de Resultados por iteracion: ")
+    for i in range(inicio+1, num_data):
+        result += ( ( (Calcular_Dividendo(k, num_veces))/(float(math.factorial(num_veces))) ) * (tabla_values[i][num_veces+1]) )
+        print("Iteracion:",(i-2),", resultado =",result)
+        num_veces = num_veces+1
+    return result
+
     #Metodo de Bisecciones
 def PoliNewton(): 
     Presentacion()
@@ -53,7 +95,9 @@ def PoliNewton():
         print("\n\n\tNo cumple con la regla de simetria de las H\n\n\tpor lo que no tiene solucion por este metodo\n\n")
     else:
         tabla_values = Contruction_Table(init_tabla, num_data)
-        print("\n\nTabla de Iteraciones:")
+        print("\n\nTabla de Calculos:")
         print(tabla_values)
-
-PoliNewton()
+        value_calcular = float(sol_Value_Calcular())
+        result = Calcular_Result(tabla_values, value_calcular, num_data)
+        print("\n\nEl resultado final para ",value_calcular," es = ",result)
+        enters(2)
